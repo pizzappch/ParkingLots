a8a08a88366683e17a0d7b7a06d5d7743be958a3
Millieme <YourName@DESKTOP-KT5UQ07>
2020-11-24 02:46:54 +0700

    add ServiceCharge

---
+++ src/parkinglot/ServiceCharge.java
@@ -0,0 +1,89 @@
+package parkinglot;
+
+import java.sql.Time;
+import java.util.Scanner;
+
+ 
+
+public class ServiceCharge {
+
+    private int costWhenOverFreeForTwoWheel = 10;
+    private int costWhenOverFreeForThreeWheel = 30;
+    private int costWhenOverFreeForFourWheel = 30;
+
+
+    public int getCostWhenOverFreeForTwoWheel() {
+        return costWhenOverFreeForTwoWheel;
+    }
+
+ 
+
+    public int getCostWhenOverFreeForThreeWheel() {
+        return costWhenOverFreeForThreeWheel;
+    }
+
+ 
+
+    public int getCostWhenOverFreeForFourWheel() {
+        return costWhenOverFreeForFourWheel;
+    }
+    
+
+    public void setCostWhenOverFreeForTwoWheel(int costWhenOverFreeForTwoWheel) {
+        this.costWhenOverFreeForTwoWheel = costWhenOverFreeForTwoWheel;
+    }
+
+ 
+
+    public void setCostWhenOverFreeForThreeWheel(int costWhenOverFreeForThreeWheel) {
+        this.costWhenOverFreeForThreeWheel = costWhenOverFreeForThreeWheel;
+    }
+
+ 
+
+    public void setCostWhenOverFreeForFourWheel(int costWhenOverFreeForFourWheel) {
+        this.costWhenOverFreeForFourWheel = costWhenOverFreeForFourWheel;
+    }
+
+ 
+
+    public float getCost(Time t, int numberWheels) {
+        //TOTAL PARKING TIME > X hour 
+        float cost = 0;
+        Time latestTime = new Time(System.currentTimeMillis());
+        System.out.println("latestTime:"+latestTime.toString());
+        System.out.println("Time of car in:"+t.toString());
+//            int h = t.getHours();
+//            int m = t.getMinutes();
+
+
+                int wn = -1;
+                Scanner sc = new Scanner(System.in);
+                while (wn != 2 && wn != 3 && wn != 4) {
+                    //sout Please Check Wheels again
+                    System.out.println("Please Check Wheels again!!");
+                    //sout  How many wheel for this car?
+                    System.out.print("How many wheel for this car?:");
+                    //input wheels number into wn variable
+                    wn = sc.nextInt();
+                    switch (wn) {
+                        case 2: cost = this.getCostWhenOverFreeForTwoWheel();
+                            break;
+
+                        case 3: cost = this.getCostWhenOverFreeForFourWheel();
+                            break;
+
+                        case 4:
+                                cost = this.getCostWhenOverFreeForFourWheel();
+                            break;
+
+                        default:                                
+                            break;
+                    }
+        }
+        return cost;
+
+ 
+
+    }
+}
